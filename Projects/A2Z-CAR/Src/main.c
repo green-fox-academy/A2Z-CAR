399a83e02cd8747b8ccbd206b375639d7170df5d
ladanyiz <ladzoltan@gmail.com>
2017-08-08 16:19:52 +0200

    feat(Zoli-dev): add wifi init

--- Projects/A2Z-CAR/Src/main.c
+++ Projects/A2Z-CAR/Src/main.c
@@ -59,6 +59,22 @@ GPIO_InitTypeDef GPIO_InitDef;
 /* Private macro -------------------------------------------------------------*/
 /* Private variables --------------------------------------------------------*/
 #define LED_TOGGLE_DELAY         (1000)
+
+#define SSID     "A66 Guest"
+#define PASSWORD "Hello123"
+
+#define WIFI_WRITE_TIMEOUT 10000
+#define WIFI_READ_TIMEOUT  10000
+#define CONNECTION_TRIAL_MAX          10
+
+uint8_t RemoteIP[] = {10, 27, 99, 110};
+uint8_t RxData [500];
+char* modulename;
+uint8_t TxData[] = "Hello big brother board!";
+uint16_t RxLen;
+uint8_t  MAC_Addr[6];
+uint8_t  IP_Addr[4];
+
 TIM_HandleTypeDef pwm_handle;
 TIM_OC_InitTypeDef pwm_oc_init;
 
@@ -71,6 +87,7 @@ static void StartThread(void const * argument);
 static void servo_control_thread(void const * argument);
 
 void system_init();
+void wifi_init();
 void pwm_init();
 void pwm_set_duty(float duty);
 void set_servo_angle(int8_t angle);
@@ -84,6 +101,8 @@ void set_servo_angle(int8_t angle);
   */
 int main(void)
 {
+	uint16_t Datalen;
+
 	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
 	HAL_Init();
 
@@ -161,7 +180,69 @@ void system_init()
 	printf("UART Printf Example: retarget the C library printf function to the UART\r\n");
 	printf("** Test finished successfully. ** \r\n");
 
+	wifi_init();
+
 	pwm_init();
+}
+
+
+void wifi_init()
+{
+	uint16_t Trials = CONNECTION_TRIAL_MAX;
+	int32_t Socket = -1;
+
+	/*Initialize  WIFI module */
+	if(WIFI_Init() ==  WIFI_STATUS_OK) {
+	    printf("> WIFI Module Initialized.\n");
+	    if(WIFI_GetMAC_Address(MAC_Addr) == WIFI_STATUS_OK) {
+	        printf("> es-wifi module MAC Address : %X:%X:%X:%X:%X:%X\n",
+	               MAC_Addr[0],
+	               MAC_Addr[1],
+	               MAC_Addr[2],
+	               MAC_Addr[3],
+	               MAC_Addr[4],
+	               MAC_Addr[5]);
+	    } else {
+	    	printf("> ERROR : CANNOT get MAC address\n");
+	        BSP_LED_On(LED2);
+	    }
+
+	    if( WIFI_Connect(SSID, PASSWORD, WIFI_ECN_WPA2_PSK) == WIFI_STATUS_OK) {
+	    	printf("> es-wifi module connected \n");
+	    	if(WIFI_GetIP_Address(IP_Addr) == WIFI_STATUS_OK) {
+	    		printf("> es-wifi module got IP Address : %d.%d.%d.%d\n",
+	    		               IP_Addr[0],
+	    		               IP_Addr[1],
+	    		               IP_Addr[2],
+	    		               IP_Addr[3]);
+
+				printf("> Trying to connect to Server: %d.%d.%d.%d:8002 ...\n",
+					   RemoteIP[0],
+					   RemoteIP[1],
+					   RemoteIP[2],
+					   RemoteIP[3]);
+		        while (Trials--) {
+		        	if( WIFI_OpenClientConnection(0, WIFI_TCP_PROTOCOL, "TCP_CLIENT", RemoteIP, 8002, 0) == WIFI_STATUS_OK) {
+						printf("> TCP Connection opened successfully.\n");
+						Socket = 0;
+		        	}
+		        }
+		        if(!Trials) {
+		            printf("> ERROR : Cannot open Connection\n");
+		            BSP_LED_On(LED2);
+		        }
+			} else {
+		        printf("> ERROR : es-wifi module CANNOT get IP address\n");
+		        BSP_LED_On(LED2);
+		    }
+		} else {
+	        printf("> ERROR : es-wifi module NOT connected\n");
+	        BSP_LED_On(LED2);
+	    }
+	} else {
+	    printf("> ERROR : WIFI Module cannot be initialized.\n");
+	    BSP_LED_On(LED2);
+	}
 }
 
 
