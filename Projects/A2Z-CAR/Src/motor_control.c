c8192fa50cf20a3740db23481d342f661ec2145d
ladanyiz <ladzoltan@gmail.com>
2017-08-14 15:19:58 +0200

    feat (Zoli-dev): add dc motor directional control

--- Projects/A2Z-CAR/Src/motor_control.c
+++ Projects/A2Z-CAR/Src/motor_control.c
@@ -2,28 +2,6 @@
 #include "cmsis_os.h"
 #include "pwm_driver.h"
 
-/*
-	P = ?
-	I = ?
-	ctrl_out_min = 0 (% PWM)
-	ctrl_out_max = 100 (% PWM)
-	error = 0;
-	integral = 0;
-
-	error = required current - measured current;
-	integral = integral + error;
-	ctrler_out = P * error + I * integral;
-
-	if (ctrler_out < ctrler_out_min) {
-		ctrler_out = ctrler_out_min;
-		integral = integral - error;
-	}
-	else if (ctrler_out > ctrler_out_max) {
-		ctrler_out = ctrler_out_max;
-		integral = integral - error;
-	}
-*/
-
 float p_value = 0.1;
 float i_value = 0.1;
 float error = 0.0;
@@ -32,7 +10,19 @@ float required_current = 0.0;
 float measured_current = 0.0;
 float ctrler_out = 0.0;
 
-float pi_control() {
+void set_direction(int8_t dir)
+{
+	if (dir == 1) {				// set forward
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+	} else if (dir == -1) {		// set reverse
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_SET);
+	}
+}
+
+float pi_control()
+{
 	error = required_current - measured_current;
 	integral += error;
 	ctrler_out = p_value * error + i_value * integral;
@@ -50,6 +40,16 @@ float pi_control() {
 
 void motor_control_thread(void const * argument)
 {
+	// Initialize pin D6 and D8 (PB1 and PB2) as output
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+	GPIO_InitDef.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitDef.Pull = GPIO_NOPULL;
+	GPIO_InitDef.Speed = GPIO_SPEED_FAST;
+	GPIO_InitDef.Pin = GPIO_PIN_1 | GPIO_PIN_2;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitDef);
+
+	// set forward
+	set_direction(1);
 
 	while(1) {
 		motor_pwm_set_duty(pi_control());
