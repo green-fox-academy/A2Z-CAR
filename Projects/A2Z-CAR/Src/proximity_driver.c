365232266a2bf36ff87f5fbcbe2267935fed7fe0
andszb <andiszabo22@gmail.com>
2017-08-24 15:25:19 +0200

    adjust delay and add error handling

--- Projects/A2Z-CAR/Src/proximity_driver.c
+++ Projects/A2Z-CAR/Src/proximity_driver.c
@@ -62,10 +62,10 @@ void proximity_send_trigger()
 {
 	//init trigger pin to D3 (PB0)
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
-	HAL_Delay(500);
+	HAL_Delay(100);
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
 	//printf("Proxim trigger sent.\n");
-	HAL_Delay(250);
+	HAL_Delay(100);
 
 }
 
@@ -98,7 +98,9 @@ int8_t proximity_control_thread()
 		proxim1_cntr = 0;
 		proxim1_up = 0;
 		proximity_send_trigger();
-		while (proxim1_up == 1);
+		while (proxim1_up == 1){
+			osDelay(1);
+		}
 		proxim1_cntr = cm_cntr;
 		printf("proxim1_cntr: %lu\n", proxim1_cntr);
 
@@ -106,13 +108,20 @@ int8_t proximity_control_thread()
 		proxim2_cntr = 0;
 		proxim2_up = 0;
 		proximity_send_trigger();
-		while (proxim2_up == 1);
+		while (proxim2_up == 1){
+			osDelay(1);
+		}
 		proxim2_cntr = cm_cntr;
 		printf("proxim2_cntr: %lu\n\n", proxim2_cntr);
 
-		if ((proxim1_cntr < 30) && (proxim2_cntr < 30)) {
-			disable_drive();
-			printf("Disable signal sent.\n");
+		if ((proxim1_cntr > 450) || (proxim2_cntr > 450)) {
+			//disable_drive();
+			//printf("Invalid proximity data.\n");
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET); 	//green led
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);	//red led
+		}else if ((proxim1_cntr < 30) && (proxim2_cntr < 30)) {
+			//disable_drive();
+			//printf("Disable signal sent.\n");
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET); 	//green led
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);	//red led
 
@@ -124,6 +133,7 @@ int8_t proximity_control_thread()
 
 		} else {
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);	//green led
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);	//red led
 		}
 
 	}
